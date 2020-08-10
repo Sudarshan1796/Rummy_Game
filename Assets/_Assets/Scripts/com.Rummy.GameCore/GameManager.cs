a4655cdc06f77c121322795ef9d113bccceda9fe
Sudarshan1796 <ysshetty96@gmail.com>
2020-08-10 12:43:30 +0530

    Minor hierarchy changes.

---
+++ Assets/_Assets/Scripts/com.Rummy.GameCore/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.Rummy.GameCore
+{
+    public class GameManager : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+    }
+}
---
+++ Assets/_Assets/Scripts/com.Rummy.Misc/GUIController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace com.Rummy.Misc
+{
+	public class GUIController : MonoBehaviour
+	{
+		[MenuItem("GUI/Anchors to Corners %[")]
+		static void AnchorsToCorners()
+		{
+			foreach (Transform transform in Selection.transforms)
+			{
+				RectTransform t = transform as RectTransform;
+				RectTransform pt = Selection.activeTransform.parent as RectTransform;
+
+				if (t == null || pt == null) return;
+
+				Vector2 newAnchorsMin = new Vector2(t.anchorMin.x + t.offsetMin.x / pt.rect.width,
+													t.anchorMin.y + t.offsetMin.y / pt.rect.height);
+				Vector2 newAnchorsMax = new Vector2(t.anchorMax.x + t.offsetMax.x / pt.rect.width,
+													t.anchorMax.y + t.offsetMax.y / pt.rect.height);
+
+				t.anchorMin = newAnchorsMin;
+				t.anchorMax = newAnchorsMax;
+				t.offsetMin = t.offsetMax = new Vector2(0, 0);
+			}
+		}
+	}
+}
