1af7a59a7bc12b8af4b4c55c3faa77c92131229b
akshith_juego <akshith.kumar@juegostudio.net>
2020-08-14 00:56:33 +0530

    Timer functinality added

--- Assets/_Assets/Scripts/com.Rummy.UI/TimerController.cs
+++ Assets/_Assets/Scripts/com.Rummy.UI/TimerController.cs
@@ -1,40 +1,110 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class TimerController : MonoBehaviour
 {
-    [SerializeField] private Image image;
+    [SerializeField] private Image insideImage;
+    [SerializeField] private Image borderImage;
+    [SerializeField] private Image TimerPanel;
+    [SerializeField] private TMP_Text timeCount;
+    [SerializeField] private Color startTimeColor;
+    [SerializeField] private Color midTimeColor;
+    [SerializeField] private Color criticalColor;
+    [SerializeField] private Color startTimeColorFade;
+    [SerializeField] private Color midTimeColorFade;
+    [SerializeField] private Color criticalColorFade;
 
+    private float previosRemainingTime;
     private float remainingTime;
     private float totalTime;
 
-    private Coroutine _updateCoroutine;
+    private bool isTimerActive;
+    private bool isStartColorSet;
+    private bool isMidColorSet;
+    private bool isCriticalColorSet;
+
+    private Action timerCompleteAction;
+    private void OnEnable()
+    {
+        StartTimer(30, null);
+    }
     public void StartTimer(float _totalTime,Action _action)
     {
         remainingTime = _totalTime;
         totalTime = remainingTime;
-        if(_updateCoroutine!=null)
-        {
-            StopCoroutine(_updateCoroutine);
-        }
-        _updateCoroutine = StartCoroutine(UpdateTimer(_action));
+        previosRemainingTime = remainingTime;
+
+        isTimerActive = true;
+        isStartColorSet = false;
+        isMidColorSet = false;
+        isCriticalColorSet = false;
+
+        timerCompleteAction = _action;
     }
 
-    private IEnumerator UpdateTimer(Action _action)
+    private void Update()
     {
-        image.fillAmount = remainingTime / totalTime;
-        yield return new WaitForSeconds(1.0f);
-        remainingTime--;
-        if(remainingTime>0)
+        Debug.Log(isTimerActive);
+        if (isTimerActive)
         {
-            _updateCoroutine = StartCoroutine(UpdateTimer(_action));
+            remainingTime = remainingTime - Time.deltaTime;
+            SetImageFillAmount();
+            SetTimerText();
+            SetTimerColor();
+            CheckTimerComplete();
         }
-        else
+    }
+
+    private void CheckTimerComplete()
+    {
+        if (remainingTime <= 0)
         {
-            _action.Invoke();
+            isTimerActive = false;
+            timerCompleteAction?.Invoke();
+            timerCompleteAction = null;
         }
+    }
+
+    private void SetImageFillAmount()
+    {
+        insideImage.fillAmount = remainingTime / totalTime;
+        borderImage.fillAmount = remainingTime / totalTime;
+    }
+
+    private void SetTimerText()
+    {
+        if (previosRemainingTime - remainingTime >= 1.0f)
+        {
+            previosRemainingTime = remainingTime;
+            timeCount.text = remainingTime.ToString("N0");
+        }
+    }
+
+    private void SetTimerColor()
+    {
+        if (remainingTime > 15.0f && !isStartColorSet)
+        {
+            SetColor(startTimeColor, startTimeColorFade, ref isStartColorSet);
+        }
+        if (remainingTime < 15.0f && remainingTime > 7.0f && !isMidColorSet)
+        {
+            SetColor(midTimeColor, midTimeColorFade, ref isMidColorSet);
+        }
+        if (remainingTime < 7.0f && !isCriticalColorSet)
+        {
+            SetColor(criticalColor, criticalColorFade, ref isCriticalColorSet);
+        }
+    }
+
+    private void SetColor(Color color, Color fadeColor, ref bool issetColor)
+    {
+        insideImage.color = fadeColor;
+        borderImage.color = color;
+        TimerPanel.color = color;
+        issetColor = true;
     }
 }
